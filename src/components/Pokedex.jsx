8d74fba441614b954cac5c6ed47ed9d3f93b612c
claudiocassimiro <days_surfing@live.com>
2021-10-07 23:40:28 -0300

    Requisitos para o exercicio completo

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -4,13 +4,61 @@ import './Pokedex.css'
 import PropTypes from 'prop-types'
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokeIndex: 0,
+            pokemons: this.props.poke.map(pokemon => pokemon),
+        };
+        this.changePoke = this.changePoke.bind(this);
+        this.typeFire = this.typeFire.bind(this);
+        this.typePsychic = this.typePsychic.bind(this);
+        this.changeAll = this.changeAll.bind(this);
+    }
+
+    changePoke() {
+        if (this.state.pokeIndex < this.state.pokemons.length - 1) {
+            this.setState((prevState) => ({
+                pokeIndex: prevState.pokeIndex + 1,
+            }));
+        } else {
+            this.setState({
+                pokeIndex: 0,
+            });
+        }
+    }
+
+    typeFire() {
+        const firePokemons = this.props.poke.filter(pokemon => pokemon.type === 'Fire')
+        this.setState({
+            pokeIndex: 0,
+            pokemons: firePokemons,
+        })
+    }
+
+    typePsychic() {
+        const psychicPokemons = this.props.poke.filter(pokemon => pokemon.type === 'Psychic')
+        this.setState({
+            pokeIndex: 0,
+            pokemons: psychicPokemons,
+        })
+    }
+
+    changeAll({ target }) {
+        this.setState({
+            pokeIndex: 0,
+            pokemons: this.props.poke,
+        })
+    }
     render() {
-        
-        const pokemons = this.props.poke;
 
         return (
             <div className="containerPokemons">
-                {pokemons.map(pokemon => <Pokemon key={pokemon.id} poke={pokemon}/>)}
+                <Pokemon poke={this.state.pokemons[this.state.pokeIndex]}/>
+                <button onClick={this.changePoke}>Próximo</button>
+                <button onClick={this.changeAll}>ALL</button>
+                <button onClick={this.typeFire}>Fire</button>
+                <button onClick={this.typePsychic}>Psychic</button>
             </div>
         );
     }
